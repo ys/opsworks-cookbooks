9ed3d0fd43877ca5a6af77da1095cb5da7d84b73
Yannick Schutz <yannick.schutz@gmail.com>
2013-09-19 16:58:20 +0200

    Initial cookbook

---
+++ env-vars-opsworks-cookbooks/CHANGELOG.md
@@ -0,0 +1,13 @@
+env-vars-cookbooks CHANGELOG
+===================
+
+This file is used to list changes made in each version of the env-vars-cookbooks cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of env-vars-cookbooks
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ env-vars-opsworks-cookbooks/recipes/default.rb
@@ -0,0 +1,23 @@
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
+  template "#{deploy[:deploy_to]}/current/config/initializers/environment_variables.rb" do
+    source "enviroment_variables.erb"
+    cookbook 'enviroment-variables'
+    group deploy[:group]
+    owner deploy[:user]
+    variables(:env_vars => deploy[:env_vars])
+
+    notifies :run, resources(:execute => "restart Rails app #{application}")
+
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}") && File.exists?("#{deploy[:deploy_to]}/current/config/")
+    end
+  end
+end
