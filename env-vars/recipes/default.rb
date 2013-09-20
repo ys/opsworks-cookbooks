d37347b9ef21a53b38c370127a69bab5e5e6dcba
Yannick Schutz <yannick.schutz@gmail.com>
2013-09-20 18:51:37 +0200

    new env vars

--- env-vars/recipes/default.rb
+++ env-vars/recipes/default.rb
@@ -7,14 +7,35 @@ node[:deploy].each do |application, deploy|
     action :nothing
   end
 
+  Chef::Log.info("Setting environment variables")
+
+  Chef::Log.info("Setting environment variables for current process")
+  deploy[:environment_variables].each do |name, value|
+    ENV["#{name}"] = "#{value}"
+  end
+
+  Chef::Log.info("Writing variables to /etc/environment to have them after restart")
   template "/etc/environment" do
     source "environment.erb"
-    cookbook 'env-vars'
-    group deploy[:group]
-    owner deploy[:user]
-    mode 0755
-    variables(:env_vars => deploy[:env_vars])
+    mode "0644"
+    owner "root"
+    group "root"
+    variables({
+      :environment_variables => deploy[:environment_variables]
+    })
+  end
 
-    notifies :run, resources(:execute => "restart Rails app #{application}")
+  Chef::Log.info("Creating shell file to export variables")
+  template "/usr/local/bin/environment.sh" do
+    source "environment.sh.erb"
+    mode "0755"
+    owner "root"
+    group "root"
+  end
+
+  Chef::Log.info("Exporting variables for every new created process")
+  execute "/usr/local/bin/environment.sh" do
+    user "root"
+    action :run
   end
 end
